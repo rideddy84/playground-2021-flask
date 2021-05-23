7c3f0254573b16f102f1e1f542a3a134e3323838
Jimin Kim M1 <rideddy84@gmail.com>
2021-05-23 23:02:46 +0900

    add coupon model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from init import make_celery
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
 app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
     CELERY_RESULT_BACKEND='redis://localhost:6379'
@@ -11,4 +11,5 @@ app.config.update(
 db = SQLAlchemy(app)
 celery = make_celery(app)
 
+import models
 import routes
--- models.py
+++ models.py
@@ -1,4 +1,11 @@
 from app import db
+from sqlalchemy.sql import func
+import enum
+
+
+class CouponType(enum.Enum):
+    AMOUNT = 1
+    PERCENT = 2
 
 
 class User(db.Model):
@@ -8,3 +15,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Coupon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    discount = db.Column(db.Float, nullable=False)
+    code = db.Column(db.String(16), unique=True, nullable=False)
+    type = db.Column(db.Enum(CouponType), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
